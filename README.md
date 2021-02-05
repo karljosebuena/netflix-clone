ee2a734bcbf6414ac85d1a678e54870d5c8c005e
Karl Jose Buena <karljosebuena@gmail.com>
2021-02-05 08:33:00 +0800

    Completed login and profile screens.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ## Netflix Clone with REACT.JS! 💯 🔥
 
 Built with:
+👉 REDUX
+👉 REACT ROUTER
 👉 AXIOS
 👉 TMDB APIs
+👉 FIREBASE AUTHENTICATION
+👉 FIREBASE HOSTING
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          email: authUser.email
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    return unsubscribe;
+  }, [dispatch])
 
   return (
     <div className="app">
@@ -19,6 +38,9 @@ function App() {
           <LoginScreen />
         ) : (
             <Switch>
+              <Route path="/profile">
+                <ProfileScreen />
+              </Route>
               <Route path="/">
                 <HomeScreen />
               </Route>
